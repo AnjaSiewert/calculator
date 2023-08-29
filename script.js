7633ba5091e22acbc4cbcb4ee0ee852e82e2e0b7
Anja Siewert <anja.siewert@live.de>
2023-08-29 13:11:15 +0200

    add Operator funcionality

--- script.js
+++ script.js
@@ -2,30 +2,56 @@ const calculatorDisplay = document.querySelector('h1');
 const inputButtons = document.querySelectorAll('button');
 const clearButton = document.getElementById('clear-button');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
+if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+} else {
     const displayValue = calculatorDisplay.textContent;
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
-
-// Add Event Listeners for numbers, operators, decimal buttons
+}
 
 function addDecimal() {
+    if (awaitingNextValue)  return;
     if(!calculatorDisplay.textContent.includes('.')) {
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
     }
 }
 
+function useOperator(operator) {
+    const currentValue = Number(calculatorDisplay.textContent);
+    if (!firstValue) {
+        firstValue = currentValue;
+    } else {
+        console.log('currentValue', currentValue);
+    }
+    awaitingNextValue = true;
+    operatorValue = operator;   
+    console.log('firstValue', firstValue);
+    console.log('operator', operatorValue);
+}
+
+// Add Event Listeners for numbers, operators, decimal buttons
+
 inputButtons.forEach((inputButton) => {
     if (inputButton.classList.length === 0) {
         inputButton.addEventListener('click', () => sendNumberValue(inputButton.value));
     } else if (inputButton.classList.contains('operator')) {
-        inputButton.addEventListener('click', () => sendNumberValue(inputButton.value));
+        inputButton.addEventListener('click', () => useOperator(inputButton.value));
     } else if (inputButton.classList.contains('decimal')) {
         inputButton.addEventListener('click', () => addDecimal());
     }
 })
 
 function clearAll() {
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
     calculatorDisplay.textContent = '0';
 }
 
