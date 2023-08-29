d12a53a654495c2f75d3ed667f43c4f4e6ec88bc
Anja Siewert <anja.siewert@live.de>
2023-08-29 11:49:10 +0200

    add decimal function

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ function sendNumberValue(number) {
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
-function clearAll() {
-    calculatorDisplay.textContent = '0';
-}
-
 // Add Event Listeners for numbers, operators, decimal buttons
+
+function addDecimal() {
+    if(!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
 
 inputButtons.forEach((inputButton) => {
     if (inputButton.classList.length === 0) {
@@ -19,8 +21,12 @@ inputButtons.forEach((inputButton) => {
     } else if (inputButton.classList.contains('operator')) {
         inputButton.addEventListener('click', () => sendNumberValue(inputButton.value));
     } else if (inputButton.classList.contains('decimal')) {
-        inputButton.addEventListener('click', () => sendNumberValue(inputButton.value));
+        inputButton.addEventListener('click', () => addDecimal());
     }
 })
+
+function clearAll() {
+    calculatorDisplay.textContent = '0';
+}
 
 clearButton.addEventListener('click', clearAll);
